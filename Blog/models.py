79818bd9d2611e3930f4bdfabc30a5e99d44aca0
Ahmad <almohammedahmed2@gmail.com>
2021-02-01 11:53:39 +0200

    back-end finished

--- Blog/models.py
+++ Blog/models.py
@@ -2,15 +2,58 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
-class Blog(models.Model):
-    first_name = models.CharField(max_length=64)
-    Last_name = models.TextField()
+
+class Skils(models.Model):
     student_number = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    image = models.URLField()
-    
+    html = models.BooleanField()
+    css = models.BooleanField()
+    js = models.BooleanField()
+    react = models.BooleanField()
+    nodejs = models.BooleanField()
+    express = models.BooleanField()
+    django = models.BooleanField()
+    heroku = models.BooleanField()
+    vercel = models.BooleanField()
+    githab_pages = models.BooleanField()
+    python = models.BooleanField()
+    postgresql = models.BooleanField()
+    mongodb = models.BooleanField()
+    git = models.BooleanField()
 
     def __str__(self):
-        return self.first_name
+        return str(self.student_number)
+
+class Projects(models.Model):
+    student_number = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=63)
+    live_url = models.URLField()
+    github_url = models.URLField()
+    description=models.TextField()
+    features = models.TextField()
+    def __str__(self):
+        return str(self.project_name)
 
 
+class Blog(models.Model):
+    student_number = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,unique=True)
+    first_name = models.CharField(max_length=64)
+    Last_name = models.CharField(max_length=64)
+    image = models.URLField(blank=True)
+    about_me = models.TextField()
+    skils = models.ForeignKey(Skils,on_delete=models.CASCADE)
+    projects = models.ManyToManyField(Projects,  through= 'Ownership')
+    email = models.EmailField(unique=True)
+    github = models.URLField()
+    linkedin = models.URLField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.Last_name)
+
+
+class Ownership(models.Model):
+    collection = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    owner = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.owner)
    
+
--- Blog/permissions.py
+++ Blog/permissions.py
@@ -2,7 +2,11 @@ from rest_framework import permissions
 
 class PermissionsClass(permissions.BasePermission):
     def has_object_permission(self,request,view,obj):
+        if obj.student_number != 1 and request.method == 'POST':
+            return False
         if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.id == 1:
             return True
         if obj.student_number == request.user:
             return True
